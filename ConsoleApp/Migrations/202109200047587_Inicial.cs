648808ab285fc11364f72bbd5e062dbacfaeb7e5
Thales Fernandes <thalesinacioo@gmail.com>
2021-09-19 22:26:12 -0300

    SendingFiles
    Just to send all the files

---
+++ ConsoleApp/Migrations/202109200047587_Inicial.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Inicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.clientes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.clientes");
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ConsoleApp
+{
+    public class Context:DbContext
+    {
+        public DbSet<cliente> cliente { get; set; }
+    }
+
+   
+
+    public class cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
